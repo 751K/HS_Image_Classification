6194544f78ae0d76c01bff98213db4b4fe7ffcdf
kong <k1012922528@163.com>
2025-03-31 17:53:23 +0800

    refactor: remove unused imports and update __all__ in __init__.py for cleaner module exports

--- src/CNNBase/__init__.py
+++ src/CNNBase/__init__.py
@@ -1,10 +1,9 @@
 # __init__.py
 
 from .LeeEtAl3D import LeeEtAl3D
-from .ResNet1D import ResNet1D
 from .ResNet2D import ResNet2D
 from .ResNet3D import ResNet3D
 from .GCN2D import GCN2D
 from .HybridSN import HybridSN
 
-__all__ = ['LeeEtAl3D', "ResNet3D", 'ResNet2D', 'ResNet1D', 'GCN2D', 'HybridSN']
+__all__ = ['LeeEtAl3D', "ResNet3D", 'ResNet2D', 'GCN2D', 'HybridSN']
--- src/TransformerBase/__init__.py
+++ src/TransformerBase/__init__.py
@@ -1,8 +1,7 @@
 # __init__.py
-from .Transformer import Transformer
 from .SwimTransformer import SwimTransformer
 from .VisionTransformer import VisionTransformer
 from .SSFTT import SSFTT
 from .SFT import SFT
 
-__all__ = ['Transformer', 'SwimTransformer', 'VisionTransformer', 'SSFTT', 'SFT']
+__all__ = ['SwimTransformer', 'VisionTransformer', 'SSFTT', 'SFT']
