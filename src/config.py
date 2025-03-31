e12e7c83fe3e7dfae93bc68cde4cc60a76128897
kong <k1012922528@163.com>
2025-03-31 17:55:27 +0800

    feat: update configuration parameters in config.py for improved model performance and dataset support

--- src/config.py
+++ src/config.py
@@ -3,7 +3,6 @@ import torch
 import inspect
 import os
 import sys
-from datetime import datetime
 
 from src.utils.device import get_device
 from src.model_init import AVAILABLE_MODELS
@@ -12,7 +11,7 @@ from src.utils.paths import create_experiment_dir
 
 class Config:
     def __init__(self):
-        self.test_mode = True
+        self.test_mode = False
 
         # 获取调用此类的文件名
         caller_frame = inspect.stack()[1]
@@ -33,30 +32,35 @@ class Config:
             self.num_epochs = 80
             # torch.autograd.set_detect_anomaly(True)
 
-        self.datasets = 'KSC'  # 可选:'Indian', 'Pavia', 'Salinas', 'KSC', 'Botswana'
+        self.datasets = 'Indian'  # 可选:'Indian', 'Pavia', 'Salinas', 'KSC', 'Botswana', 'Wuhan'
 
         self.num_workers = 0
 
         self.seed = 3407
 
         self.feature_dim = 128
-        self.expand = 32
+        self.expand = 8
         self.depth = 1
         self.dropout = 0.25
+        self.d_conv = 16
+
+        self.chunk_size = 16
 
         if self.datasets == 'Indian':
-            self.batch_size = 32
-            self.test_size = 0.85
+            self.batch_size = 64
+            self.test_size = 0.8
             self.num_classes = 16
             self.mlp_dim = 64
-            self.expand = 64
+            self.expand = 32
             self.d_state = 48
-            self.dropout = 0.1824152805122036
-            self.learning_rate = 0.0008420354656015855
-            self.warmup_ratio = 0.06176653864344813
-            self.cycles = 0.5988120910182956
-            self.min_lr_ratio = 0.0468285619606516
-            self.weight_decay = 2.767587061278965e-05
+            self.d_conv = 8
+            self.head_dim = 4
+            self.dropout = 0.4519834690833574
+            self.learning_rate = 0.0002684994708259589
+            self.warmup_ratio = 0.04006485849074962
+            self.cycles = 0.6310212386924017
+            self.min_lr_ratio = 0.12814831606095006
+            self.weight_decay = 0.00014499372875857687
         elif self.datasets == 'Pavia':
             self.batch_size = 64
             self.test_size = 0.95
@@ -84,14 +88,15 @@ class Config:
             self.num_classes = 16
         elif self.datasets == 'KSC':
             self.batch_size = 32
-            self.mlp_dim = 64
-            self.dropout = 0.47419792880823625
-            self.d_state = 16
-            self.learning_rate = 0.0007942357117505358
-            self.warmup_ratio = 0.018587729415862373
-            self.cycles = 0.5031109547446925
-            self.min_lr_ratio = 0.09210873093398604
-            self.weight_decay = 0.00018084984175502887
+            self.mlp_dim = 32
+            self.dropout = 0.29942626972917785
+            self.d_state = 80
+            self.expand = 64
+            self.learning_rate = 0.000317246216733878
+            self.warmup_ratio = 0.03542440054169354
+            self.cycles = 0.8386501740444608
+            self.min_lr_ratio = 0.015853889118795032
+            self.weight_decay = 0.0006216143401161145
             self.test_size = 0.75
             self.num_classes = 13
         elif self.datasets == 'Botswana':
@@ -104,8 +109,21 @@ class Config:
             self.cycles = 0.5004982345432989
             self.min_lr_ratio = 0.0828985932694622
             self.weight_decay = 0.0004960078104701533
-            self.test_size = 0.65
+            self.test_size = 0.7
             self.num_classes = 14
+        elif self.datasets == 'Wuhan':
+            self.batch_size = 64
+            self.mlp_dim = 32
+            self.dropout = 0.129859960550872
+            self.d_state = 64
+            self.cycles = 0.50
+            self.expand = 8
+            self.learning_rate = 0.0005144888086719854
+            self.warmup_ratio = 0.010269880544951123
+            self.min_lr_ratio = 0.1855273798968476
+            self.weight_decay = 0.00015654784836898332
+            self.test_size = 0.95
+            self.num_classes = 22
         else:
             raise ValueError(f"Unsupported dataset: {self.datasets}. Supported datasets are: 'Indian', 'Pavia', "
                              f"'Salinas', 'KSC', 'Botswana'")
@@ -139,7 +157,7 @@ class Config:
             is_main=(caller_filename == 'main.py' and not self.test_mode)
         )
 
-        self.optuna_trials = 40  # Optuna 试验次数
+        self.optuna_trials = 60  # Optuna 试验次数
 
     @staticmethod
     def select_model():
